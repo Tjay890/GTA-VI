c814d2fb9540bbbe9063ed59574b08fe080f0edd
Tjay890 <157789242+Tjay890@users.noreply.github.com>
2024-03-20 09:06:43 +0000

    cords

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ pygame.display.set_caption("GTA VI(Leak)")
 done = False
 clock= pygame.time.Clock()
 
+WHITE = (200, 200, 200)
+WINDOW_HEIGHT = 800
+WINDOW_WIDTH = 800
+
+
+
+def drawGrid():
+      blockSize = 20 #Set the size of the grid block
+      for x in range(0, WINDOW_WIDTH, blockSize):
+          for y in range(0, WINDOW_HEIGHT, blockSize):
+              rect = pygame.Rect(x, y, blockSize, blockSize)
+              pygame.draw.rect(screen, WHITE, rect, 1)
 while not done:
       screen.fill((255,255,255))
       bordrect = bord.get_rect()
@@ -79,3 +91,6 @@ world = {
     }
   }
 }
+
+
+#vakjes Casino(546, 200), Nightclub(436, 184), Freakshop(501, 279), Barber(439, 450), homies crip(392, 318), Beach(188, 369), Docks(556, 662), house(234, 262), Airport(270, 573), Cayo perigo(114, 454)
