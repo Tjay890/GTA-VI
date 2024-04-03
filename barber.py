285174f952fc8d3ae77682b9c778c865de6875ed
Lil-Johny <157789264+Lil-Johny@users.noreply.github.com>
2024-04-03 08:44:31 +0000

    barber

--- barber.py
+++ barber.py
@@ -2,21 +2,20 @@ from location import *
 
 
 class haircut:
+
   def hairstyle():
     print("Do want a haircut ma G?")
     a = input(">")
     if a == "No" or a == "no":
-      return
+      style = "YEYEASSHAIRCUT"
+      return style
     else:
       print("Wich haircut do you want ma n***a!")
       print("You can chose between these 5 n***a!")
       print("Buzz, Fringe, Mullet, Bold or Default")
       hair = str(input(">"))
-      while (hair != "Buzz" and 
-        hair != "Fringe" and 
-        hair != "Mullet" and 
-        hair != "Bold" and 
-        hair != "Default"):
+      while (hair != "Buzz" and hair != "Fringe" and hair != "Mullet"
+             and hair != "Bold" and hair != "Default"):
         print("You cannot chose that n***a !\nChoose again!")
         print("Wich haircut do you want ma n***a!")
         print("You can chose between these 5 n***a!")
@@ -24,17 +23,11 @@ class haircut:
         hair = str(input(">"))
       return hair
 
-
   hairshit = hairstyle()
   print("Yo hairstyle is now", hairshit, "N***a!")
 
 
-
-
 class Barber(Location):
+
   def __init__(self, name, up, right, down, left):
     super().__init__(name, up, right, down, left)
-
-
-
-  
