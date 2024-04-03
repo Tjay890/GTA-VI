6743bae11bc24b83924e8ef8076f2b8aca9cbc76
MPpennings <161813291+MPpennings@users.noreply.github.com>
2024-04-03 08:45:05 +0000

    enbter regel toegevoegd

--- location.py
+++ location.py
@@ -7,5 +7,6 @@ class Location():
     self.left = left
   def printdirections(self):
     print("You are currently at " + self.name)
+    print("You can enter this place by pressing [ENTER] or,")
     print("You can go:\n", "Up:",self.up,"\n","Right:",self.right,"\n","Down:",self.down,"\n","Left:",self.left)
   
