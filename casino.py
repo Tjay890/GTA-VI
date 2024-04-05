5b984c26fd5e6b99988ab0e2b7b75a4af41e657f
MPpennings <161813291+MPpennings@users.noreply.github.com>
2024-04-05 10:31:05 +0000

    endless casino patch

--- casino.py
+++ casino.py
@@ -4,6 +4,7 @@ from player import *
 
 class Casino_games():
   @staticmethod
+  #geheim pinapparaat waar je je cash kan opladen
   def pinapparaat():
     print("This is the secret ATM you can withdraw any amount of money you want")
     print("You have", Stats.cash,"money.")
@@ -11,21 +12,33 @@ class Casino_games():
     Stats.cash += extra
   
   @staticmethod
+  #Kop of munt minigame
   def coinflip():
     print("You have", Stats.cash, "money.")
-    bet = int(input("How much do you want to bet\n"))
-    while bet > Stats.cash:
-      print("You dont have that much money.")
-      bet = int(input("How much do you want to bet\n"))
+    #inzet
+    bet = input("How much do you want to bet. (Type a number or all in)\n")
+    #kijken of je all in doet en anders je inzet in een nummer veranderen
+    if bet == "all in":
+      bet = Stats.cash
+    else:
+      bet = int(bet)
+      #checken of je inzet niet meer is dan je hebt
+      while bet > Stats.cash:
+        print("You dont have that much money.")
+        bet = input("How much do you want to bet. (Type a number or all in)\n")
+    
+    #geld dat je ingezet hebt van je totale geld afhalen
     Stats.cash -= bet
     
     print("heads or tails?")
     
     choise = input(">")
+    #zorgen dat je een goede keus maakt
     while choise != "heads" and choise != "tails":
       print("Dat kan je niet kiezen")
       choise = input("Kies opnieuw:\n")
     coin = random.randint(0,1)
+    #checken of jouw keuze gelijk is aan wat de uitkomst was
     if (choise == "heads" and coin == 0) or (choise == "tails" and coin == 1):
       Stats.cash += bet*2
       print("You win your money is doubled.")
@@ -35,28 +48,37 @@ class Casino_games():
       
     
   @staticmethod
+  #roulette spel
   def roulette():
     print("roulette shit")
   
   @staticmethod
+  #binnenkomst bij casino
   def entrance():
+    exit = False
     print("Welcome to the ls casino")
-    print("you have", Stats.cash, "cash")
-    print("You have 1 game you can play.")
-    print("""Our current games that are available are:
-    1. Heads or Tails.
-    2. Roulette.
-    """)
     
-    choise = input("Choose your game. (press 1 or 2)\n")
-    if choise == "1":
-      Casino_games.coinflip()
-    elif choise == "2":
-      Casino_games.roulette()
-    elif choise == "69420":
-      Casino_games.pinapparaat()
-    else:
-      print("That is not a game you are going outside")
+    while exit is False:
+      
+      print("you have", Stats.cash, "cash")
+      print("You have 1 game you can play.")
+      print("""Our current games that are available are:
+      1. Heads or Tails.
+      2. Roulette.
+      """)
+      
+      choise = input("Choose your game. (press 1 or 2)\n")
+      if choise == "1":
+        Casino_games.coinflip()
+      elif choise == "2":
+        Casino_games.roulette()
+      elif choise == "3":
+        exit = True
+        print("You are going outside.")
+      elif choise == "69420":
+        Casino_games.pinapparaat()
+      else:
+        print("That is not a game")
       
   
   
