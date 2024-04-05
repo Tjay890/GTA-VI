f53463091a8aa9872d9928d93c499a381e0599bc
Tjay890 <157789242+Tjay890@users.noreply.github.com>
2024-04-05 11:17:35 +0000

    chcisk ideeen toegevoegt

--- beach.py
+++ beach.py
@@ -3,3 +3,5 @@ from location import *
 class Beach(Location):
   def __init__(self, name, up, right, down, left, xcoord, ycoord):
     super().__init__(name, up, right, down, left, xcoord, ycoord)
+#blonde(x2 heatlh), brunette(x2 both), redhead(fucked in ass and dies), black(lost half money)
+#bald(moet zelf ook bald zijn and +1000 cash), blackhair(x2 attack)
